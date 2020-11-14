dd2a1e86b2998c9aa023e0dd8e315022ab83ae82
Dave Wysochanski <dwysocha@redhat.com>
2020-11-14 18:21:40 -0500

    NFS: Convert to the netfs API and nfs_readpage to use netfs_readpage
    This patch converts the main NFS read paths to the new netfs API,
    when fscache is enabled, and converts readpage while minimizing
    changes to the existing NFS read code paths.
    
    The netfs API requires a few functions to be provided by the
    netfs:
    - init_rreq: allows netfs to allocate resources prior to IO
    - is_cache_enabled: allows netfs to disable fscache
    - begin_cache_operation: signals the start of an fscache IO
    - issue_op: called when netfs should issue read to server
    - clamp_length: allows netfs to limit size of IO
    - cleanup: allows netfs to cleanup after an IO is complete
    
    The new netfs_readpage() API is called when fscache is enabled.
    If a read cannot be satisfied from fscache, the netfs is called
    back via issue_op() to obtain the data from the server.  Once
    the read completes, the netfs must call netfs_subreq_terminated()
    which then may write the data to fscache.  In order to call back
    into fscache via netfs_subreq_terminated(), we must save the
    netfs_read_subrequest* as a field in the nfs_pgio_header, similar
    to nfs_direct_req.
    
    If the netfs has a read IO limit (for example, NFS 'rsize' mount
    options) the clamp_length() function is called.
    
    Signed-off-by: Dave Wysochanski <dwysocha@redhat.com>

--- fs/nfs/pagelist.c
+++ fs/nfs/pagelist.c
@@ -52,6 +52,7 @@ void nfs_pgheader_init(struct nfs_pageio_descriptor *desc,
 	hdr->good_bytes = mirror->pg_count;
 	hdr->io_completion = desc->pg_io_completion;
 	hdr->dreq = desc->pg_dreq;
+	hdr->fsc = desc->pg_fsc;
 	hdr->release = release;
 	hdr->completion_ops = desc->pg_completion_ops;
 	if (hdr->completion_ops->init_hdr)
@@ -833,6 +834,7 @@ void nfs_pageio_init(struct nfs_pageio_descriptor *desc,
 	desc->pg_lseg = NULL;
 	desc->pg_io_completion = NULL;
 	desc->pg_dreq = NULL;
+	desc->pg_fsc = NULL;
 	desc->pg_bsize = bsize;
 
 	desc->pg_mirror_count = 1;
